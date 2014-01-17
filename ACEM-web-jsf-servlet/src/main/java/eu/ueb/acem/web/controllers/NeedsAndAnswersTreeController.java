1cf15fd0239da4f16e03c07abee08a80912a960c
Grégoire Colbert <gc.github-com@artefarita.com>
2014-01-17 11:35:14 +0100

    trying to debug the f tree

--- ACEM-web-jsf-servlet/src/main/java/eu/ueb/acem/web/controllers/NeedsAndAnswersTreeController.java
+++ ACEM-web-jsf-servlet/src/main/java/eu/ueb/acem/web/controllers/NeedsAndAnswersTreeController.java
@@ -26,6 +26,8 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
 import org.primefaces.context.RequestContext;
+import org.primefaces.event.NodeCollapseEvent;
+import org.primefaces.event.NodeExpandEvent;
 import org.primefaces.event.NodeSelectEvent;
 import org.primefaces.event.TreeDragDropEvent;
 import org.primefaces.model.DefaultTreeNode;
@@ -64,142 +66,188 @@ public class NeedsAndAnswersTreeController extends AbstractContextAwareControlle
 
 	private TreeNode selectedNode;
 	
+	private TreeNode expandedNode;
+
 	public NeedsAndAnswersTreeController() {
 	}
 
-  @PostConstruct
-  public void initTree() {
+	@PostConstruct
+	public void initTree() {
 		logger.info("entering initTree");
-   	//editableTreeBean.setVisibleRootLabel(getString("NEEDS_AND_ANSWERS.TREE.ROOT.LABEL"));
+		//editableTreeBean.setVisibleRootLabel(getString("NEEDS_AND_ANSWERS.TREE.ROOT.LABEL"));
 		editableTreeBean.setVisibleRootLabel("Besoins");
 		Set<Besoin> needs = needsAndAnswersService.getNeedsWithParent(null);
 		logger.info("[NeedsAndAnswersTreeController.initTree] Fetched {} pedagogical needs at root of tree.", needs.size());
 		for (Besoin need : needs) {
 			logger.info("need = {}", need.getName());
-	    	createTree(need, editableTreeBean.getVisibleRoot());
+			createTree(need, editableTreeBean.getVisibleRoot());
 		}
 		editableTreeBean.getVisibleRoot().setExpanded(true);
-    	logger.info("leaving initTree");
+		logger.info("leaving initTree");
 		logger.info("------");
-    }
+	}
 
-    /**
-     * Recursive function to construct Tree
-     */
-    public void createTree(Besoin besoin, TreeNode rootNode) {
-        TreeNode newNode = new DefaultTreeNode(new Menu(besoin.getId(), besoin.getName()), rootNode);
-        Set<Besoin> childrenNodes = needsAndAnswersService.getNeedsWithParent(besoin);
-        for (Besoin besoinChild : childrenNodes) {
-            createTree(besoinChild, newNode);
-        }
-    }
-    
+	/**
+	 * Recursive function to construct Tree
+	 */
+	public void createTree(Besoin besoin, TreeNode rootNode) {
+		TreeNode newNode = new DefaultTreeNode(new Menu(besoin.getId(), besoin.getName()), rootNode);
+		Set<Besoin> childrenNodes = needsAndAnswersService.getNeedsWithParent(besoin);
+		for (Besoin besoinChild : childrenNodes) {
+			createTree(besoinChild, newNode);
+		}
+	}
+
 	public EditableTreeBean getTreeBean() {
 		return editableTreeBean;
 	}
-	
+
 	public TreeNode getTreeRoot() {
 		return editableTreeBean.getRoot();
 	}
 
-    public TreeNode getSelectedNode() {  
-			return selectedNode;  
-    }
+	public TreeNode getSelectedNode() {  
+		return selectedNode;  
+	}
 
-    public void setSelectedNode(TreeNode selectedNode) {
-    	logger.info("entering setSelectedNode({})", selectedNode);
-    	if (this.selectedNode != null) {
-    		if (this.selectedNode != selectedNode) {
-    			if (this.selectedNode.isSelected()) {
-    				this.selectedNode.setSelected(false);
-    			}
-    			if (! this.selectedNode.getChildren().contains(selectedNode)) {
-		    		// We recursively collapse the children of this.selectedNode
-		        	collapseChildrenOf(this.selectedNode);
-		        	// We collapse the currently selected node itself
-		        	this.selectedNode.setExpanded(false);
-		        	// We collect in a Set the parents of the newly selected node
-		        	Set<TreeNode> selectedNodeParents = new HashSet<TreeNode>();
-		        	TreeNode selectedNodeParent = selectedNode.getParent();
-		        	while (selectedNodeParent != null) {
-		        		selectedNodeParents.add(selectedNodeParent);
-		        		selectedNodeParent = selectedNodeParent.getParent();
-		        	}
-		        	// We collapse all of this.selectedNode parents, until we find the grand-parent
-		        	// that has the currently selected node and the newly selected node as grand-children
-		    		TreeNode currentParent = this.selectedNode.getParent();
-		    		while ((currentParent != null)
-		    				&& (! selectedNodeParents.contains(currentParent))) {
-		    			currentParent.setExpanded(false);
-		    			currentParent = currentParent.getParent();
-		    		}
-    			}
-    		}
-    	}
-			if (this.selectedNode == selectedNode) {
-				if (this.selectedNode.getChildCount() > 0) {
-					this.selectedNode.setExpanded(! this.selectedNode.isExpanded());
+	public void setSelectedNode(TreeNode selectedNode) {
+		this.selectedNode = selectedNode;
+	}
+
+	public void setExpandedNode(TreeNode expandedNode) {
+		logger.info("entering setExpandedNode({})", expandedNode);
+		/*
+		if (this.expandedNode != null) {
+			this.expandedNode.setExpanded(! this.expandedNode.isExpanded());
+			if (this.expandedNode != expandedNode) {
+				if (! this.expandedNode.getChildren().contains(expandedNode)) {
+					// We recursively collapse the children of this.expandedNode
+					collapseChildrenOf(this.expandedNode);
+					// We collapse the currently selected node itself
+					this.expandedNode.setExpanded(false);
 				}
 			}
 			else {
-				if (! selectedNode.isSelected()) {
-					selectedNode.setSelected(true);
-					selectedNode.setExpanded(true);
-				}
-        this.selectedNode = selectedNode;
+				this.expandedNode.setExpanded(! this.expandedNode.isExpanded());
 			}
-			logger.info("leaving setSelectedNode({})", selectedNode);
-			logger.info("------");
-    }
-
-    public void onNodeSelect(NodeSelectEvent event) {
-    	logger.info("onNodeSelect");
-    	FacesMessage message = null;
-    	if (selectedNode != null) {
-    		message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Was selected", selectedNode.toString());
-            FacesContext.getCurrentInstance().addMessage(null, message);      	
-    	}
-    	setSelectedNode(event.getTreeNode());
-    	if (selectedNode != null) {
-    		message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Is now selected", selectedNode.toString());
-            FacesContext.getCurrentInstance().addMessage(null, message);      	
-    	}
-    }
-    
-    public void displaySelectedSingle() {  
-    	if(selectedNode != null) {  
-    		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected", selectedNode.getData().toString());
-    		FacesContext.getCurrentInstance().addMessage(null, message);
-    	}
-    }
-
-    private void collapseChildrenOf(TreeNode node) {
-    	logger.info("entering collapseChildrenOf, node={}", node.getData().toString());
-    	for (TreeNode child : node.getChildren()) {
-    		child.setExpanded(false);
-    		if (! child.isLeaf()) {
-    			collapseChildrenOf(child);
-    		}
-    	}
-    	logger.info("leaving collapseChildrenOf, node={}", node.getData().toString());
-    }
-
-    public void addChildToSelectedNode() {
-    	logger.info("entering addChildToSelectedNode, selectedNode={}", selectedNode.getData().toString());
-    	collapseChildrenOf(selectedNode);
-    	TreeNode newNode = editableTreeBean.addChild(selectedNode, null, "Nouveau");
-    	setSelectedNode(newNode);
-    	saveSelectedNode();
-    	logger.info("leaving addChildToSelectedNode, selectedNode={}", selectedNode.getData().toString());
+		}
+		*/
+		this.expandedNode = expandedNode;
+		logger.info("leaving setExpandedNode({})", expandedNode);
 		logger.info("------");
-    }
+	}
 
-    public void associateAnswerToSelectedNode() {
-    	logger.info("entering associateAnswerToSelectedNode, selectedNode={}", selectedNode.getData().toString());
-    	
-    	logger.info("leaving associateAnswerToSelectedNode, selectedNode={}", selectedNode.getData().toString());
+	/*
+	public void setSelectedNode(TreeNode selectedNode) {
+		logger.info("entering setSelectedNode({})", selectedNode);
+		if (this.selectedNode != null) {
+			if (this.selectedNode != selectedNode) {
+				if (this.selectedNode.isSelected()) {
+					this.selectedNode.setSelected(false);
+				}
+				if (! this.selectedNode.getChildren().contains(selectedNode)) {
+					// We recursively collapse the children of this.selectedNode
+					collapseChildrenOf(this.selectedNode);
+					// We collapse the currently selected node itself
+					this.selectedNode.setExpanded(false);
+					// We collect in a Set the parents of the newly selected node
+					Set<TreeNode> selectedNodeParents = new HashSet<TreeNode>();
+					TreeNode selectedNodeParent = selectedNode.getParent();
+					while (selectedNodeParent != null) {
+						selectedNodeParents.add(selectedNodeParent);
+						selectedNodeParent = selectedNodeParent.getParent();
+					}
+					// We collapse all of this.selectedNode parents, until we find the grand-parent
+					// that has the currently selected node and the newly selected node as grand-children
+					TreeNode currentParent = this.selectedNode.getParent();
+					while ((currentParent != null)
+							&& (! selectedNodeParents.contains(currentParent))) {
+						currentParent.setExpanded(false);
+						currentParent = currentParent.getParent();
+					}
+				}
+			}
+		}
+		if (this.selectedNode == selectedNode) {
+			if (this.selectedNode.getChildCount() > 0) {
+				this.selectedNode.setExpanded(! this.selectedNode.isExpanded());
+			}
+		}
+		else {
+			if (! selectedNode.isSelected()) {
+				selectedNode.setSelected(true);
+				selectedNode.setExpanded(true);
+			}
+			this.selectedNode = selectedNode;
+		}
+		logger.info("leaving setSelectedNode({})", selectedNode);
 		logger.info("------");
-    }
+	}
+	*/
+
+	public void onNodeSelect(NodeSelectEvent event) {
+		logger.info("onNodeSelect");
+		FacesMessage message = null;
+		if (selectedNode != null) {
+			message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Was selected", selectedNode.toString());
+			FacesContext.getCurrentInstance().addMessage(null, message);      	
+		}
+		setSelectedNode(event.getTreeNode());
+		if (selectedNode != null) {
+			message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Is now selected", selectedNode.toString());
+			FacesContext.getCurrentInstance().addMessage(null, message);      	
+		}
+	}
+
+	public void onNodeExpand(NodeExpandEvent event) {
+		logger.info("onNodeExpand");
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Expanded",
+				((Menu) event.getTreeNode().getData()).toString());
+		FacesContext.getCurrentInstance().addMessage(null, message);
+		// TODO : replace this "true" with "if user is not admin"
+		if (true) {
+			setExpandedNode(event.getTreeNode());
+		}
+	}
+
+	public void onNodeCollapse(NodeCollapseEvent event) {
+		logger.info("onNodeCollapse");
+	}
+
+	public void displaySelectedSingle() {  
+		if(selectedNode != null) {  
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected", selectedNode.getData().toString());
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+	}
+
+	private void collapseChildrenOf(TreeNode node) {
+		logger.info("entering collapseChildrenOf, node={}", node.getData().toString());
+		for (TreeNode child : node.getChildren()) {
+			child.setExpanded(false);
+			if (! child.isLeaf()) {
+				collapseChildrenOf(child);
+			}
+		}
+		logger.info("leaving collapseChildrenOf, node={}", node.getData().toString());
+	}
+
+	public void addChildToSelectedNode() {
+		logger.info("entering addChildToSelectedNode, selectedNode={}", selectedNode.getData().toString());
+		collapseChildrenOf(selectedNode);
+		TreeNode newNode = editableTreeBean.addChild(selectedNode, null, getString("NEEDS_AND_ANSWERS.TREE.NEW_NEED_LABEL"));
+		setSelectedNode(newNode);
+		saveSelectedNode();
+		logger.info("leaving addChildToSelectedNode, selectedNode={}", selectedNode.getData().toString());
+		logger.info("------");
+	}
+
+	public void associateAnswerToSelectedNode() {
+		logger.info("entering associateAnswerToSelectedNode, selectedNode={}", selectedNode.getData().toString());
+
+		logger.info("leaving associateAnswerToSelectedNode, selectedNode={}", selectedNode.getData().toString());
+		logger.info("------");
+	}
 
 	public void saveSelectedNode() {
 		if (selectedNode != null) {
@@ -208,13 +256,13 @@ public class NeedsAndAnswersTreeController extends AbstractContextAwareControlle
 			Besoin savedBesoin;
 			if ((selectedNode.getParent() != null) && (selectedNode.getParent().getData() != null)) {
 				savedBesoin = needsAndAnswersService.createOrUpdateNeed(((Menu)selectedNode.getData()).getId(),
-											((Menu) selectedNode.getData()).getLabel(),
-											((Menu) selectedNode.getParent().getData()).getId());
+						((Menu) selectedNode.getData()).getLabel(),
+						((Menu) selectedNode.getParent().getData()).getId());
 			}
 			else {
 				savedBesoin = needsAndAnswersService.createOrUpdateNeed(((Menu)selectedNode.getData()).getId(),
-											((Menu) selectedNode.getData()).getLabel(),
-											null);
+						((Menu) selectedNode.getData()).getLabel(),
+						null);
 			}
 			((Menu)selectedNode.getData()).setId(savedBesoin.getId());
 			// TODO sinon si selectedNode est une Reponse
@@ -226,38 +274,38 @@ public class NeedsAndAnswersTreeController extends AbstractContextAwareControlle
 		logger.info("------");
 	}
 
-    public void deleteSelectedNode() {
+	public void deleteSelectedNode() {
 		logger.info("entering deleteSelectedNode, selectedNode={}", selectedNode.getData().toString());
 		if (selectedNode != null) {
-        	if (selectedNode.getChildCount() == 0) {
-        		needsAndAnswersService.deleteNeed(((Menu) selectedNode.getData()).getId());
-		    	selectedNode.getChildren().clear();
-		    	selectedNode.getParent().getChildren().remove(selectedNode);  
-		    	selectedNode.setParent(null);
-		    	this.selectedNode = null;
-	    	}
-	    	else {
-        		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-        				getString("NEEDS_AND_ANSWERS.TREE.CONTEXT_MENU.DELETE_NODE.HAS_CHILDREN_ERROR.TITLE"),
-        				getString("NEEDS_AND_ANSWERS.TREE.CONTEXT_MENU.DELETE_NODE.HAS_CHILDREN_ERROR.DETAILS"));
-        		RequestContext.getCurrentInstance().update("messages");
-        		FacesContext.getCurrentInstance().addMessage(null, message);
-	    		logger.info("The selected node has children, cannot delete!");
-	    	}
-    	}
+			if (selectedNode.getChildCount() == 0) {
+				needsAndAnswersService.deleteNeed(((Menu) selectedNode.getData()).getId());
+				selectedNode.getChildren().clear();
+				selectedNode.getParent().getChildren().remove(selectedNode);  
+				selectedNode.setParent(null);
+				this.selectedNode = null;
+			}
+			else {
+				FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+						getString("NEEDS_AND_ANSWERS.TREE.CONTEXT_MENU.DELETE_NODE.HAS_CHILDREN_ERROR.TITLE"),
+						getString("NEEDS_AND_ANSWERS.TREE.CONTEXT_MENU.DELETE_NODE.HAS_CHILDREN_ERROR.DETAILS"));
+				RequestContext.getCurrentInstance().update("messages");
+				FacesContext.getCurrentInstance().addMessage(null, message);
+				logger.info("The selected node has children, cannot delete!");
+			}
+		}
 		logger.info("leaving deleteSelectedNode");
 		logger.info("------");
-    }
-    
-    public void onDragDrop(TreeDragDropEvent event) {
-        TreeNode dragNode = event.getDragNode();  
-        TreeNode dropNode = event.getDropNode();  
-        int dropIndex = event.getDropIndex();  
+	}
 
-    	needsAndAnswersService.changeParentOfNeed(((Menu)dragNode.getData()).getId(),((Menu)dropNode.getData()).getId());
-        
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Dragged " + dragNode.getData(), "Dropped on " + dropNode.getData() + " at " + dropIndex);  
-        FacesContext.getCurrentInstance().addMessage(null, message);  
-    }  
+	public void onDragDrop(TreeDragDropEvent event) {
+		TreeNode dragNode = event.getDragNode();  
+		TreeNode dropNode = event.getDropNode();  
+		int dropIndex = event.getDropIndex();  
+
+		needsAndAnswersService.changeParentOfNeed(((Menu)dragNode.getData()).getId(),((Menu)dropNode.getData()).getId());
+
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Dragged " + dragNode.getData(), "Dropped on " + dropNode.getData() + " at " + dropIndex);  
+		FacesContext.getCurrentInstance().addMessage(null, message);  
+	}  
 
 }
